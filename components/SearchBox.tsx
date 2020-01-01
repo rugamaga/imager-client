ebc7a930110d87542a7145261aa18d89afa79790
rugamaga <rugamaga@gmail.com>
2020-01-01 18:31:30 +0900

    refactoring SearchBox

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export const SearchBox = ({value, onInput}) => (
+export const SearchBox = ({value, onChange}) => (
   <form noValidate onSubmit={() => false} className="searchbox sbx">
     <div role="search" className="sbx__wrapper">
-      <input type="search" name="search" value={value} onInput={onInput} placeholder="tag search" autoComplete="off" required={false} className="sbx__input" />
+      <input
+        type="search"
+        name="search"
+        defaultValue={value}
+        onChange={onChange}
+        placeholder="tag search"
+        autoComplete="off"
+        required={false}
+        className="sbx__input"
+      />
     </div>
   </form>
 );
