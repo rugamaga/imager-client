fc6d4382bb81fa80938af7da40528c88127e596b
rugamaga <rugamaga@gmail.com>
2020-01-01 20:24:54 +0900

    Delete old codes

--- src/SearchBox.js
+++
@@ -1,9 +0,0 @@
-import {h} from "hyperapp";
-
-export const SearchBox = ({value, oninput}) => (
-  <form novalidate="novalidate" onsubmit="return false;" class="searchbox sbx">
-    <div role="search" class="sbx__wrapper">
-      <input type="search" name="search" value={value} oninput={oninput} placeholder="tag search" autocomplete="off" required="required" class="sbx__input" />
-    </div>
-  </form>
-);
