13e7b05eda94b6a572b3cc9d87ed1c4dedc93a38
rugamaga <rugamaga@gmail.com>
2020-01-01 20:59:51 +0900

    label can response as label as radio buttons

--- components/RadioList.tsx
+++ components/RadioList.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-const RadioInput = ({group, name, checked, onChange}) =>
+const RadioInput = ({id, group, name, checked, onChange}) =>
   <span>
-    <input type="radio" name={group} value={name} checked={checked} onChange={onChange} />
-    <label>{name}</label>
+    <input type="radio" id={id} name={group} value={name} checked={checked} onChange={onChange} />
+    <label htmlFor={id}>{name}</label>
   </span>
 
 export const RadioList = ({group, names, active, onChange}) =>
   <div>
     {
       names
-        .map( (name) =>
+        .map( (name) => [name, `${group}_${name}`] )
+        .map( ([name, id]) =>
           <RadioInput
-            key={`${group}_${name}`}
+            key={id}
+            id={id}
             group={group}
             name={name}
             checked={name == active}
